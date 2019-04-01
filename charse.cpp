430becb58f2ed338692a98d6c91c1f26c802e9fd
khoaxxx238 <48320778+khoaxxx238@users.noreply.github.com>
2019-04-01 12:26:01 +0700

    Update charse.cpp

--- charse.cpp
+++ charse.cpp
@@ -25,49 +25,40 @@ geometry_msgs::Twist getMessage(double linear_x, double angular_z)
 
 void move()
 {
-    //if()
-    if (abs(t_a - theta) > pi / 4 && abs(t_a - theta) < 2 * pi - pi / 4)
-    // if (t_a < 0.1 || t_a > 6.1)
-    //     pub.publish(getMessage(1.5 * target, 0 * (t_a - theta)));
-    // else
+    if (target > 0.0001)
     {
-        pub.publish(getMessage(0, 2 * a_z));
-        cout << "1" << endl;
-    }
-    else
-    {
-        if (abs(target) > 0.01)
+        if (target > 0.1)
         {
-            if (abs(target) > 0.1)
-                if (abs(t_a - theta) > 0.16)
-                {
-                    pub.publish(getMessage(2.4 * target, 0.2 * a_z));
-                    cout << "2" << endl;
-                }
-                else
-                {
-                    pub.publish(getMessage(2 * target, 0.2 * a_z));
-                    cout << "3" << endl;
-                }
+            if (abs(t_a - theta) > pi / 4 && abs(t_a - theta) < 2 * pi - pi / 4)
+            {
+                pub.publish(getMessage(0, 2 * a_z / abs(a_z)));
+                //cout << "1" << endl;
+            }
             else
             {
-                if (abs(t_a - theta) > 0.016)
-                {
-                    pub.publish(getMessage(1 * target, 0.05 * a_z));
-                    cout << "4" << endl;
-                }
-                else
-                {
-                    pub.publish(getMessage(0.2 * target, 0.02 * a_z));
-                    cout << "5" << endl;
-                }
+                pub.publish(getMessage(2, 1 * a_z / abs(a_z)));
+                //cout << "2" << endl;
             }
         }
         else
         {
-            pub.publish(getMessage(0, 0));
-            state = 0;
+            if (abs(t_a - theta) > 0.00001)
+            {
+                pub.publish(getMessage(0, 1 * a_z / abs(a_z)));
+                //cout << "3" << endl;
+            }
+            else
+            {
+                pub.publish(getMessage(target, 0));
+                //cout << "4" << endl;
+            }
         }
+    }
+    else
+    {
+        pub.publish(getMessage(0, 0));
+        //cout << "0" << endl;
+        state = 0;
     }
 }
 
@@ -83,7 +74,8 @@ void poseCallback(const turtlesim::Pose::ConstPtr &msg)
     //          << "vt=" << vt << endl
     //          << endl;
     // };
-
+    if (theta < 0)
+        theta = 2 * pi + theta;
     target = sqrt((tx - x) * (tx - x) + (ty - y) * (ty - y));
     target_angle = atan2(ty - y, tx - x);
 
@@ -120,6 +112,10 @@ void poseCallback(const turtlesim::Pose::ConstPtr &msg)
         else
             a_z = t_a - theta;
     }
+    cout << a_z << endl
+         << target_angle << endl
+         << theta << endl
+         << endl;
 }
 
 void poseCallback2(const turtlesim::Pose::ConstPtr &msg)
